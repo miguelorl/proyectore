f837d99dda9091a57c69f9638bf7d7f055ac9566
miguelorl <miguel.orland@gmail.com>
2025-06-07 19:31:47 -0500

    Cambio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -12,5 +13,8 @@ def index():
     conn.close()
     return render_template('index.html', items=items)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    if not os.path.exists(app.config ['UPLOAD_FOLDER']):
+        os.makedirs(app.config [ 'UPLOAD_FOLDER'])
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
